709f27a092117af0ce00ddf76371501cbe37d24f
Paul Johnston <pcj@stack.build>
2025-12-14 10:37:23 -0700

    refactor: extract settings components into separate module
    Extract settings-related components and templates from monolithic app
    files into dedicated settings.js and settings.soy files.
    
    JavaScript:
    - Extract SettingsSelect component (settings page with navigation)
    - Extract SettingsAppearanceComponent (theme preference UI)
    - Move LocalStorageKey enum to settings module
    - Add centrl.settings module with proper exports
    
    Soy templates:
    - Extract settingsSelect template (settings page layout)
    - Extract settingsAppearanceComponent template (theme UI)

--- app/bcr/app.js
+++ app/bcr/app.js
@@ -43,21 +43,14 @@ const { MvsDependencyTree } = goog.require('centrl.mvs_tree');
 const { SafeHtml, sanitizeHtml } = goog.require('google3.third_party.javascript.safevalues.index');
 const { SearchComponent } = goog.require('centrl.search');
 const { SelectNav } = goog.require('centrl.SelectNav');
-const { aspectInfoComponent, bodySelect, bzlFileSourceComponent, docsMapComponent, docsMapSelectNav, docsSelect, documentationInfoListComponent, documentationInfoSelect, documentationReadmeComponent, fileErrorBlankslate, fileInfoListComponent, fileInfoSelect, fileInfoTreeComponent, functionInfoComponent, homeOverviewComponent, homeSelect, loadInfoComponent, macroInfoComponent, maintainerComponent, maintainersMapComponent, maintainersMapSelectNav, maintainersSelect, moduleBlankslateComponent, moduleExtensionInfoComponent, moduleSelect, moduleVersionBlankslateComponent, moduleVersionComponent, moduleVersionDependenciesComponent, moduleVersionDependentsComponent, moduleVersionList, moduleVersionSelectNav, moduleVersionsFilterSelect, modulesMapSelect, modulesMapSelectNav, navItem, providerInfoComponent, registryApp, repositoryRuleInfoComponent, ruleInfoComponent, ruleMacroInfoComponent, settingsAppearanceComponent, settingsSelect, symbolInfoComponent, symbolTypeName, toastSuccess, valueInfoComponent } = goog.require('soy.centrl.app');
+const { SettingsSelect } = goog.require('centrl.settings');
+const { aspectInfoComponent, bodySelect, bzlFileSourceComponent, docsMapComponent, docsMapSelectNav, docsSelect, documentationInfoListComponent, documentationInfoSelect, documentationReadmeComponent, fileErrorBlankslate, fileInfoListComponent, fileInfoSelect, fileInfoTreeComponent, functionInfoComponent, homeOverviewComponent, homeSelect, loadInfoComponent, macroInfoComponent, maintainerComponent, maintainersMapComponent, maintainersMapSelectNav, maintainersSelect, moduleBlankslateComponent, moduleExtensionInfoComponent, moduleSelect, moduleVersionBlankslateComponent, moduleVersionComponent, moduleVersionDependenciesComponent, moduleVersionDependentsComponent, moduleVersionList, moduleVersionSelectNav, moduleVersionsFilterSelect, modulesMapSelect, modulesMapSelectNav, navItem, providerInfoComponent, registryApp, repositoryRuleInfoComponent, ruleInfoComponent, ruleMacroInfoComponent, symbolInfoComponent, symbolTypeName, toastSuccess, valueInfoComponent } = goog.require('soy.centrl.app');
 const { copyToClipboardButton, moduleVersionsListComponent } = goog.require('soy.registry');
 const { setElementInnerHtml } = goog.require('google3.third_party.javascript.safevalues.dom.elements.element');
 
 
 const HIGHLIGHT_SYNTAX = true;
 const FORMAT_MARKDOWN = true;
-
-/**
- * @enum {string}
- */
-const LocalStorageKey = {
-    COLOR_MODE: "bcr-color-mode",
-};
-
 
 /**
  * @enum {string}
@@ -500,150 +493,6 @@ class HomeOverviewComponent extends Component {
     }
 }
 
-
-
-class SettingsSelect extends ContentSelect {
-    /**
-     * @param {?dom.DomHelper=} opt_domHelper
-     */
-    constructor(opt_domHelper) {
-        super(opt_domHelper);
-    }
-
-    /**
-     * @override
-     */
-    createDom() {
-        this.setElementInternal(soy.renderAsElement(settingsSelect, {
-        }, {
-            pathUrl: this.getPathUrl(),
-        }));
-    }
-
-    /**
-     * @override
-     */
-    enterDocument() {
-        super.enterDocument();
-
-        this.addTab(TabName.APPEARANCE, new SettingsAppearanceComponent(this.dom_));
-    }
-
-    /**
-     * @override
-     * @param {!Route} route
-     */
-    goHere(route) {
-        this.select(TabName.APPEARANCE, route.add(TabName.APPEARANCE));
-    }
-}
-
-
-
-class SettingsAppearanceComponent extends Component {
-    /**
-     * @param {?dom.DomHelper=} opt_domHelper
-     */
-    constructor(opt_domHelper) {
-        super(opt_domHelper);
-
-        /** @private @type {?HTMLSelectElement} */
-        this.themeSelectEl_ = null;
-    }
-
-    /**
-     * @override
-     */
-    createDom() {
-        this.setElementInternal(soy.renderAsElement(settingsAppearanceComponent));
-    }
-
-    /**
-     * @override
-     */
-    enterDocument() {
-        super.enterDocument();
-
-        this.enterThemeSelect();
-    }
-
-    /**
-     * @override
-     */
-    exitDocument() {
-        this.themeSelectEl_ = null;
-        super.exitDocument();
-    }
-
-    enterThemeSelect() {
-        this.themeSelectEl_ = /** @type {!HTMLSelectElement} */(this.getCssElement('theme'));
-
-        let colorMode = this.getLocalStorageColorMode();
-        if (colorMode) {
-            this.setDocumentColorMode(colorMode);
-        } else {
-            colorMode = this.getDocumentColorMode();
-            this.setLocalStorageColorMode(colorMode);
-        }
-        this.themeSelectEl_.value = colorMode;
-
-        this.getHandler().listen(
-            this.themeSelectEl_,
-            events.EventType.CHANGE,
-            this.handleThemeSelectChange,
-        );
-    }
-
-    /**
-     * @param {!events.BrowserEvent=} e
-     */
-    handleThemeSelectChange(e) {
-        const colorMode = this.themeSelectEl_.value || 'auto';
-        this.setDocumentColorMode(colorMode);
-        this.setLocalStorageColorMode(colorMode);
-    }
-
-    /**
-     * @returns {string} 
-     */
-    getDocumentColorMode() {
-        return this.themeSelectEl_.ownerDocument.documentElement.getAttribute('data-color-mode') || 'auto';
-    }
-
-    /**
-     * @param {string} colorMode 
-     */
-    setDocumentColorMode(colorMode) {
-        this.themeSelectEl_.ownerDocument.documentElement.setAttribute('data-color-mode', colorMode);
-    }
-
-    /**
-     * @returns {?string} 
-     */
-    getLocalStorageColorMode() {
-        return window.localStorage?.getItem(LocalStorageKey.COLOR_MODE);
-    }
-
-    /**
-     * @param {string} colorMode 
-     */
-    setLocalStorageColorMode(colorMode) {
-        if (window.localStorage) {
-            window.localStorage.setItem(LocalStorageKey.COLOR_MODE, colorMode);
-        }
-    }
-
-    /**
-     * @param {string} cssName
-     * @return {!HTMLElement}
-     */
-    getCssElement(cssName) {
-        return /** @type {!HTMLElement} */ (
-            dom.getRequiredElementByClass(cssName, this.getElementStrict())
-        );
-    }
-
-}
 
 
 class ModuleBlankslateComponent extends Component {
