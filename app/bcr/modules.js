d5c90935fc2ec941fcd9a31a071b2638843b0896
Paul Johnston <pcj@stack.build>
2026-01-25 19:01:09 -0700

    fix class and style names to prevent property renaming

--- app/bcr/modules.js
+++ app/bcr/modules.js
@@ -990,22 +990,22 @@ class ModuleVersionDependentsComponent extends ContentComponent {
 	renderDependentsMatrix(container, data) {
 		// Wrapper for horizontal scroll with grab cursor
 		const wrapper = dom.createDom("div", {
-			class: "m-1",
-			style: "overflow-x: scroll; cursor: grab;",
-			onmousedown: /** @this {!HTMLElement} */ function () {
+			"class": "m-1",
+			"style": "overflow-x: scroll; cursor: grab;",
+			"onmousedown": /** @this {!HTMLElement} */ function () {
 				this.style.cursor = "grabbing";
 			},
-			onmouseup: /** @this {!HTMLElement} */ function () {
+			"onmouseup": /** @this {!HTMLElement} */ function () {
 				this.style.cursor = "grab";
 			},
-			onmouseleave: /** @this {!HTMLElement} */ function () {
+			"onmouseleave": /** @this {!HTMLElement} */ function () {
 				this.style.cursor = "grab";
 			},
 		});
 
 		const table = dom.createDom("table", {
-			class: "width-full p-0",
-			style: "border-collapse: collapse;",
+			"class": "width-full p-0",
+			"style": "border-collapse: collapse;",
 		});
 
 		// Header row
@@ -1013,8 +1013,8 @@ class ModuleVersionDependentsComponent extends ContentComponent {
 		const headerRow = dom.createDom("tr");
 
 		const moduleHeader = dom.createDom("th", {
-			class: "text-left p-1 pr-2 position-sticky",
-			style: "z-index: 2;",
+			"class": "text-left p-1 pr-2 position-sticky",
+			"style": "z-index: 2;",
 		});
 		dom.appendChild(headerRow, moduleHeader);
 
@@ -1022,7 +1022,7 @@ class ModuleVersionDependentsComponent extends ContentComponent {
 			const headerContent = dom.createDom(
 				"div",
 				{
-					style:
+					"style":
 						"writing-mode: vertical-rl; transform: rotate(180deg); white-space: nowrap; min-height: 100px; display: flex; align-items: left; justify-content: left;",
 				},
 				version,
@@ -1031,7 +1031,7 @@ class ModuleVersionDependentsComponent extends ContentComponent {
 			const th = dom.createDom(
 				"th",
 				{
-					class: "text-left text-small p-1 pl-2",
+					"class": "text-left text-small p-1 pl-2",
 				},
 				headerContent,
 			);
@@ -1065,15 +1065,15 @@ class ModuleVersionDependentsComponent extends ContentComponent {
 			const versionText = dom.createDom(
 				"span",
 				{
-					class: "mr-1 text-light text-small",
+					"class": "mr-1 text-light text-small",
 				},
 				latestVersion,
 			);
 			const moduleLink = dom.createDom(
 				"a",
 				{
-					href: `/#/modules/${moduleName}/${latestVersion}`,
-					class: "Box-row-link",
+					"href": `/#/modules/${moduleName}/${latestVersion}`,
+					"class": "Box-row-link",
 				},
 				[versionText, moduleNameText],
 			);
@@ -1081,8 +1081,8 @@ class ModuleVersionDependentsComponent extends ContentComponent {
 			const moduleCell = dom.createDom(
 				"td",
 				{
-					class: "p-1 pr-2 position-sticky text-right",
-					style: "left: 0;",
+					"class": "p-1 pr-2 position-sticky text-right",
+					"style": "left: 0;",
 				},
 				moduleLink,
 			);
@@ -1097,8 +1097,8 @@ class ModuleVersionDependentsComponent extends ContentComponent {
 				const cell = dom.createDom(
 					"td",
 					{
-						class: cellClasses,
-						style: "max-width: 2em; width: 2em;",
+						"class": cellClasses,
+						"style": "max-width: 2em; width: 2em;",
 					},
 					isAtFront ? "•" : "",
 				);
